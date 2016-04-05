38bd7b7c1081c1603cd671695f76b99085675507
Lucas <lucas.gambini@epitech.eu>
2016-04-05 12:20:30 +0200

    .h

--- include/lamin.h
+++ include/lamin.h
@@ -5,5 +5,28 @@
 ** Login   <gambin_l@epitech.net>
 **
 ** Started on  Tue Apr  5 10:42:53 2016 Lucas Gambini
-** Last update Tue Apr  5 10:42:54 2016 Lucas Gambini
+** Last update Tue Apr  5 12:19:58 2016 Lucas Gambini
 */
+
+#ifndef _LAMIN_
+# define _LAMIN_
+
+# include "my.h"
+
+typedef struct s_node
+{
+  int		id; /* C'est le premier chiffre d'une ligne */
+  int		posx; /* Le 2e */
+  int		posy; /* Le 3e */
+  int		ant; /* si ya une fourmi ou pas */
+  struct s_node	*prev; /* Starfoullah */
+  t_next	*next; /* list chainée de next */
+}		t_node;
+
+typedef struct	s_next
+{
+  struct s_next	*nxt;
+  t_node	*next;
+}		t_next;
+
+#endif /* _LAMIN_ */

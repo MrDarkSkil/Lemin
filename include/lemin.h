57df02f220fffef4657828956ce726cd65da0b2b
Lucas <lucas.gambini@epitech.eu>
2016-04-05 10:43:02 +0200

    pushed

---
+++ include/lemin.h
@@ -0,0 +1,9 @@
+/*
+** lemin.h for lamin in /home/gambin_l/rendu/Lemin
+**
+** Made by Lucas Gambini
+** Login   <gambin_l@epitech.net>
+**
+** Started on  Tue Apr  5 10:42:53 2016 Lucas Gambini
+** Last update Tue Apr  5 10:42:54 2016 Lucas Gambini
+*/
---
+++ src/main.c
@@ -0,0 +1,9 @@
+/*
+** main.c for main in /home/gambin_l/rendu/Lemin
+**
+** Made by Lucas Gambini
+** Login   <gambin_l@epitech.net>
+**
+** Started on  Tue Apr  5 10:42:43 2016 Lucas Gambini
+** Last update Tue Apr  5 10:42:44 2016 Lucas Gambini
+*/

496487f3cd1111ca9f4d71894b930511d6927497
Lucas <lucas.gambini@epitech.eu>
2016-04-23 09:07:15 +0200

    algo

--- src/algo/initAlgo.c
+++ src/algo/initAlgo.c
@@ -1,29 +1,31 @@
 /*
-** initAlgo.c for initAlgo in /home/hubert_i/rendu/2015/CPE/Lemin/src/algo
+0;136;0c** initAlgo.c for initAlgo in /home/hubert_i/rendu/2015/CPE/Lemin/src/algo
 **
 ** Made by Léo Hubert
 ** Login   <hubert_i@epitech.net>
 **
 ** Started on  Sat Apr 23 03:49:01 2016 Léo Hubert
-** Last update Sat Apr 23 06:19:46 2016 Léo Hubert
+** Last update Sat Apr 23 09:06:58 2016 Lucas Gambini
 */
 
 # include	"lemin.h"
 
 
-int		algo(t_graph *list, t_node *sommet)
+t_node		*algo(t_graph *list, t_node *sommet)
 {
-  t_node	*fils;
+  t_link	*tmp;
 
-  fils = sommet->nxt->link;
-  sommet->nxt->father = sommet;
   sommet->visited = true;
-  printf("%s\n", sommet->id);
-  while (fils != NULL)
+  if (sommet == list->end)
+    return (sommet);
+  tmp = sommet->nxt;
+  while (tmp != NULL)
     {
-      fils = sommet->nxt->next_link->link;
-      if (fils->visited == false)
-	algo(list, fils);
+      tmp->father = sommet;
+      sommet = tmp->link;
+      if (sommet->visited == false)
+	return (algo(list, sommet));
+      tmp = tmp->next_link;
     }
   return (0);
 }
@@ -31,8 +33,12 @@ int		algo(t_graph *list, t_node *sommet)
 int		initAlgo(t_graph *list)
 {
   t_node	*start;
+  t_node	*result;
 
   start = list->start;
-  algo(list, start);
+  result = algo(list, start);
+  printf("id: %s\n", result->id);
+  result = result->nxt->father;
+  printf("id: %s\n", result->id);
   return (0);
 }
